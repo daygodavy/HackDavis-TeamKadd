99d0856548b89c6a6476cd7138a9466da3465465
Kaito Yoshida <58441279+volatio@users.noreply.github.com>
2020-01-19 13:21:00 -0800

    Global counter added

--- Listener/Listener/Controller/MoodRatingViewController.swift
+++ Listener/Listener/Controller/MoodRatingViewController.swift
@@ -120,6 +120,17 @@ class MoodRatingViewController: UIViewController {
             let users = self.rootRef.child("users")
             users.child(uid).child("MoodRating").updateChildValues([now : moodRating])
         }
+        let global = self.rootRef.child("global")
+        global.queryOrdered(byChild: "mood").queryEqual(toValue: String(Int(moodRating))).observeSingleEvent(of: DataEventType.value) { (snapshot) in
+            var total = 0.0
+                for child in snapshot.children {
+                    let snap = child as! DataSnapshot
+                    let dict = snap.value as! [String: Any]
+                    total += dict["count"] as? Double ?? 0.0
+                }
+            let input = self.rootRef.child("global").child("num"+String(Int(moodRating)))
+            input.updateChildValues(["count" : Int(total+1.0)])
+        }
         // SEGUE HERE
         navigateToHomeVC()
     }
